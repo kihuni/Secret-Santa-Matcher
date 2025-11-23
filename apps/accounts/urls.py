128b321fe109e012742e95f194bb0bbc932c4cc2
kihuni <stephenkihuni55@gmail.com>
2025-11-23 08:27:40 +0300

    fixed logout and added templates

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'accounts'
@@ -7,5 +8,5 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('profile/', views.profile, name='profile'),
     path('logout/', views.logout_view, name='logout'),
-
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
 ]
